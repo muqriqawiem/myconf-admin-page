e727b95c84dacc91f51401851fcfce00ca0eb594
muqriqawiem <muhammadmuqri32@gmail.com>
2025-01-05 11:25:52 +0800

    confirmation before delete users and conferences

--- src/context/AuthProvider.tsx
+++ src/context/AuthProvider.tsx
@@ -1,13 +1,19 @@
-"use client"
-import { SessionProvider } from "next-auth/react"
-import React from "react"
+"use client";
+import { SessionProvider } from "next-auth/react";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import React from "react";
+
+//create a queryclient instance
+const queryClient = new QueryClient();
 
 export default function AuthProvider({
   children
 }:{children:React.ReactNode}) {
   return (
     <SessionProvider>
+      <QueryClientProvider client={queryClient}>
       {children}
+      </QueryClientProvider>
     </SessionProvider>
-  )
+  );
 }
